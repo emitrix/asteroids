4ca4b9ee17b47fb1a855c0930ce6b50a595c070b
emitrix <jaimegaona@gmail.com>
2025-01-30 15:00:58 -0500

    moved the logic of player shoot to shoot method

--- player.py
+++ player.py
@@ -39,16 +39,18 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
-            if (self.shoot_cooldown <= 0):
-                self.shoot()
+            self.shoot()
     
     def move(self,dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
     
     def shoot(self):
+        if self.shoot_cooldown > 0:
+            return
+        self.shoot_cooldown = PLAYER_SHOOT_COOLDOWN
+        
         shoot = Shoot(self.position.x, self.position.y)
         shoot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
-        self.shoot_cooldown = PLAYER_SHOOT_COOLDOWN
         #print ("Shoot!")
         
